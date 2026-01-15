4d5773f803d215552073a6e99927e4c2bfbe7a59
DamianSoroka <damiansoroka2004@gmail.com>
2026-01-15 03:17:02 +0100

    v0.0

---
+++ src/main/java/net/dejak/hybrid_tools/HybridTools.java
@@ -0,0 +1,15 @@
+package net.dejak.hybrid_tools;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HybridTools implements ModInitializer {
+	public static final String MOD_ID = "hybrid-tools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/dejak/hybrid_tools/HybridToolsClient.java
@@ -0,0 +1,10 @@
+package net.dejak.hybrid_tools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HybridToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
